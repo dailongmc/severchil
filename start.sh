0c4c6754f2465f1cd4799b787f726c33d2a1f82a
dailongmc <sauff3070@gmail.com>
2026-03-22 18:14:59 +0700

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "🚀 Booting Minecraft Server..."
+
+java -Xms512M -Xmx1400M \
+-XX:+UseG1GC \
+-XX:+ParallelRefProcEnabled \
+-XX:MaxGCPauseMillis=50 \
+-XX:+UnlockExperimentalVMOptions \
+-XX:+DisableExplicitGC \
+-XX:+AlwaysPreTouch \
+-XX:G1NewSizePercent=30 \
+-XX:G1MaxNewSizePercent=40 \
+-XX:G1HeapRegionSize=8M \
+-XX:G1ReservePercent=20 \
+-XX:G1HeapWastePercent=5 \
+-XX:G1MixedGCCountTarget=4 \
+-XX:InitiatingHeapOccupancyPercent=15 \
+-XX:G1MixedGCLiveThresholdPercent=90 \
+-XX:G1RSetUpdatingPauseTimePercent=5 \
+-XX:SurvivorRatio=32 \
+-XX:+PerfDisableSharedMem \
+-XX:MaxTenuringThreshold=1 \
+-jar server.jar nogui
