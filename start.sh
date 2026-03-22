3332d244f6a1b7d5488a1989355785b10a90dcb7
dailongmc <sauff3070@gmail.com>
2026-03-22 18:22:49 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,24 +1,18 @@
 #!/bin/bash
 
-echo "🚀 Booting Minecraft Server..."
+echo "🚀 Starting Minecraft Server + Playit..."
 
+# chạy playit (tunnel)
+./playit --secret cbb26801209a07baa311a356af80072acc78161547ba24d0a3d8f7fb25eb6ccd &
+
+# đợi playit connect
+sleep 5
+
+# chạy server minecraft
 java -Xms512M -Xmx1400M \
 -XX:+UseG1GC \
 -XX:+ParallelRefProcEnabled \
 -XX:MaxGCPauseMillis=50 \
 -XX:+UnlockExperimentalVMOptions \
 -XX:+DisableExplicitGC \
--XX:+AlwaysPreTouch \
--XX:G1NewSizePercent=30 \
--XX:G1MaxNewSizePercent=40 \
--XX:G1HeapRegionSize=8M \
--XX:G1ReservePercent=20 \
--XX:G1HeapWastePercent=5 \
--XX:G1MixedGCCountTarget=4 \
--XX:InitiatingHeapOccupancyPercent=15 \
--XX:G1MixedGCLiveThresholdPercent=90 \
--XX:G1RSetUpdatingPauseTimePercent=5 \
--XX:SurvivorRatio=32 \
--XX:+PerfDisableSharedMem \
--XX:MaxTenuringThreshold=1 \
 -jar server.jar nogui
